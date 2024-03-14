1e2a7c8178780154077c5b55239b7e3c9ff3941f
sanyam238 <82820362+sanyam238@users.noreply.github.com>
2024-03-14 19:23:32 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function purchase(){
+    window.location = "buy.html";
+}
+
+function buy(){
+ document.getElementById("main_text").innerHTML = "Thanks for buying! You are truly the sigma rizzler ohio skibidi gyatt that got fanum taxed by kai cenat🤫🧏‍♂️!"
+}
